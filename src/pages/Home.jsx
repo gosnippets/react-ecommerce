42aeefef474afdbafb004274e65eea95c10a1c7f
Ramdas Gupta <er.rdgupta@gmail.com>
2023-03-04 18:11:02 +0530

    Ecom

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Grid, Link, Typography } from '@mui/material';
 import React, { Fragment, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { fetchProduct, selectProductError, selectProductList, selectProductStatus } from '../redux/slice/ProductSlice';
 
 export default function Home() {
   const status = useSelector(selectProductStatus);
   const productList = useSelector(selectProductList);
   const error = useSelector(selectProductError);
+
+  const navigate = useNavigate()
 
   const dispatch = useDispatch();
 
@@ -21,13 +25,31 @@ export default function Home() {
 
   return (
     <Fragment>
-      {productList && productList.map((product) => (
-        <div>
-          <h2>{product.title}</h2>
-          <h2>{product.description}</h2>
-          <img src={product.image} alt="img" height="200" width="150" />
-        </div>
-      ))}
+      <Grid container columns={12}>
+        {productList && productList.map((product) => (
+          <Grid xs={4} key={product.id}>
+            {/* <h2>{product.title}</h2>
+            <h2>{product.description}</h2>
+            <img src={product.image} alt="img" height="200" width="150" /> */}
+            <Card sx={{ maxWidth: 345 }} style={{ margin: "5px" }}>
+              <CardMedia
+                sx={{ height: 400 }}
+                image={product.image}
+                title={product.title}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">{product.title}</Typography>
+                <Typography variant="body2" color="text.secondary">{product.description}</Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant="contained">Add to Cart</Button>
+                <Button variant="outlined" onClick={() => navigate(`/item/${product.id}`)}>More Details</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+        ))}
+      </Grid>
     </Fragment>
   )
 }
--- src/redux/slice/ProductSlice.js
+++ src/redux/slice/ProductSlice.js
@@ -6,9 +6,14 @@ export const fetchProduct = createAsyncThunk('product/fetch', async () => {
     return response.data;
 })
 
+export const fetchSigleProduct = createAsyncThunk('product/fetchSingleProduct', async (id) => {
+    const response = await axios.get("https://fakestoreapi.com/products/"+id)
+    return response.data;
+})
+
 const productSlice = createSlice({
     name: "product",
-    initialState: { productList: [], status: 'idle', error: null },
+    initialState: { productList: [], productDetials:{}, status: 'idle', error: null },
     reducers: {
     },
     extraReducers:(builder)=>{
@@ -21,16 +26,33 @@ const productSlice = createSlice({
             state.status = 'failed'
             state.error = action.error.message
         })
+
+        builder.addCase(fetchSigleProduct.pending, (state)=>{
+            state.status = 'loading'
+        }).addCase(fetchSigleProduct.fulfilled, (state,action)=>{
+            state.status = 'success'
+            state.productDetials = action.payload
+        }).addCase(fetchSigleProduct.rejected, (state,action)=>{
+            state.status = 'failed'
+            state.error = action.error.message
+        })
     }
 });
 
+// fetchProduct
 export const selectProductStatus = (state) => state.product.status
 export const selectProductList = (state) => state.product.productList
 export const selectProductError = (state) => state.product.error
+
+// fetchSigleProduct
+export const selectSingleProductStatus = (state) => state.product.status
+export const selectSingleProductDetails = (state) => state.product.productDetials
+export const selectSingleProductError = (state) => state.product.error
 
 export default productSlice.reducer;
 
 // https://fakestoreapi.com/products/categories
 // https://fakestoreapi.com/products
+// https://fakestoreapi.com/products/2
 // https://fakestoreapi.com/products/category/electronics
 // https://fakestoreapi.com/products/category/women's%20clothing
